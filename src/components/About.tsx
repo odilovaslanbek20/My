be1daad79c17d696194d122965ffee8fa1ce65e3
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2026-01-11 23:08:42 -0800

    ""

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,22 @@
 import { useTranslation } from "react-i18next"
 
 export default function About() {
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     return (
         <>
-         <section className="bg-[#D5FF3F] flex items-center justify-center py-7 rounded-4xl">
-            <div className="w-[90%] text-black text-[26px] font-light">
-               {t('about_text')}<span className="font-bold">{t('about_text1')}</span>{t('about_text2')}<span className="font-bold">{t('about_text3')}</span>{t('about_text4')}
-            </div>
-         </section>
+            <section className="">
+                <div className="bg-[#161B22] rounded-2xl w-full py-10 my-3.5">
+                    <div className="w-[90%] flex items-center justify-between m-auto">
+                        <h1 className="text-4xl text-[#ffff] font-black mb-2.5">{t('about')}</h1>
+                        <a href="/about" className="bg-[#242938]  rounded-[10px] text-[18px] font-semibold text-[#D5FF3F] py-2 px-4">
+                            <div className="">More...</div>
+                        </a>
+                    </div>
+                </div>
+                <div className="text-[#161B22] text-[26px] font-light leading-relaxed bg-[#D5FF3F] rounded-2xl py-8">
+                    <p className="w-[90%] m-auto">{t('about_text')}<span className="font-bold">{t('about_text1')}</span>{t('about_text2')}<span className="font-bold">{t('about_text3')}</span>{t('about_text4')}</p>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,13 @@ export default function Hero() {
     const {t} = useTranslation()
     return (
         <>
-         <section className="pt-10">
-            <div className="">
+         <section className="pt-0 mb-40">
+            <img src="/starts.png" alt="start" className="w-full h-auto -mt-[100px] max-[900px]:-mt-[50px] max-[500px]:-mt-[10px] max-[450px]:-mt-[20px]"/>
+           <div className="translate-y-[-70%] max-[1024px]:translate-y-[-60%] max-[885px]:translate-y-[-50%] max-[740px]:translate-y-[-40%] max-[600px]:translate-y-[-20%] max-[500px]:translate-y-[-10%]">
                 <div className="flex items-center justify-center flex-col gap-4">
-                    <p className="text-[32px] font-medium">{t('hero_hi')}</p>
-                    <p className="text-[150px] leading-35 uppercase font-black">{t('name')}<span className="text-white">{t('name1')}</span></p>
-                    <p className="text-[24px] font-medium">{t('hero_text')}<span className="text-white">{t('hero_text1')}</span>{t('hero_text2')}</p>
+                    <p className="text-[32px] font-medium max-[400px]:text-[24px]">{t('hero_hi')}</p>
+                    <p className="text-[100px] max-[400px]:text-[50px] max-[1230px]:text-[80px] max-[740px]:text-[60px] max-[1230px]:leading-15 max-[400px]:leading-10 leading-25 uppercase font-black">{t('name')}<span className="text-white">{t('name1')}</span></p>
+                    <p className="text-[24px] max-[400px]:text-[20px] font-medium w-lg max-[530px]:w-full text-center">{t('hero_text')}<span className="text-white">{t('hero_text1')}</span>{t('hero_text2')}</p>
                     <div className="flex items-center gap-3">
                         <a target="blan_" href="https://www.linkedin.com/in/aslanbek-odilov-27136a378/">
                             <FaLinkedin size={50}/>
